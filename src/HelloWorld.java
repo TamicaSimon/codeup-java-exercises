b2f3791f317345b0051f36eddb94149f641b066a
Tamica Simon <tamica.simon@gmail.com>
2023-03-24 20:11:29 +0000

    Java Intro

---
+++ src/HelloWorld.java
@@ -0,0 +1,43 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.print("Hello, World!");
+
+        int myFavoriteNumber = 10;
+        System.out.println(myFavoriteNumber);
+
+        String myString = "Tamica SImon";
+        System.out.println(myString);
+
+        double myNumber = 3.14;
+        System.out.println(myNumber);
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+//* One evaluates the incremented value while the other evaluates the incremented value*
+
+        String theNumberThree = "three";
+        Object o = theNumberThree;
+//        int three = (int) o;
+        int three = (int) "three";
+
+//int x = 4;
+//x = x + 5; (x+=5)
+
+//int x = 3;
+//int y = 4;
+//y = y * x; (y*=x)
+
+//int x = 10;
+//int y = 2;
+//x = x / y; (x/=y)
+//y = y - x; (y-=x)
+
+    }
+}
+
