7d914e6f71e5b697a4ac9245d3bfc0fc839bbfd0
Tamica Simon <tamica.simon@gmail.com>
2023-03-28 14:27:49 +0000

    FizzBuzz

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,5 +1,6 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
+//        DO WHILE LOOPS
 //        int num = 100;
 //        do {
 //            System.out.println(num);
@@ -15,17 +16,43 @@ public class ControlFlowExercises {
 //        }
 //        while (num <= 1000000);
 //        }
+
 //        FOR LOOPS
 //        for (int i = 5; i <= 15; i++) {
 //            System.out.println(i);
 //        }
-        for (int num = 100; num >= -10; num-=5) {
-            System.out.println(num);
+//        for (int num = 100; num >= -10; num-=5) {
+//            System.out.println(num);
+//        }
+
+//        FIZZBUZZ
+//        int num = 1;
+//        do {
+//            System.out.println(num);
+//            num++;
+//
+//        } while (num <=100);
+
+        for (int num = 1; num <=100; num++){
+            if (num % 3 ==0){
+                System.out.println("Fizz");
+            } else if (num % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (num % 3 ==0 && num % 5 == 0) {
+                System.out.println("FizzBuzz");
+            } else {
+                System.out.println(num);
+            }
         }
-    }
+        }
 }
 
 
-//For
-
-//        Refactor the previous two exercises to use a for loop instead.
+//Fizzbuzz
+//
+//        One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+//
+//        Write a program that prints the numbers from 1 to 100.
+//        For multiples of three: print “Fizz” instead of the number.
+//        For the multiples of five: print “Buzz”.
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
