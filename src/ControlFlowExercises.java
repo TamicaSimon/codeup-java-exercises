e0413cbf72d3f474b7247ba9d8b114c23cbeee25
Tamica Simon <tamica.simon@gmail.com>
2023-03-28 15:38:12 +0000

    Grade Converter

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -50,30 +50,64 @@ public class ControlFlowExercises {
 //        }
 //}
 
-//        TABLE OF POWERS
+////        TABLE OF POWERS
+//        Scanner input = new Scanner(System.in);
+//        String choice;
+//        do {
+//        System.out.println("Enter an integer: ");
+//        int num = input.nextInt();
+//
+//        System.out.println("Number\tSquare\tCube");
+//        for (int i=1; i <= num; i++) {
+//            int square = i * i;
+//            int cube = i * i * i;
+//            System.out.println(i + "\t" + square + "\t" + cube);
+//        }
+//           System.out.println("Do you want to continue (Yes/No)");
+//            choice = input.next();
+//        } while (choice.equalsIgnoreCase("Yes"));
+//        }
+//    }
+
+
+//Convert given number grades into letter grades.
+
         Scanner input = new Scanner(System.in);
         String choice;
         do {
-        System.out.println("Enter an integer: ");
-        int num = input.nextInt();
+            System.out.println("Enter a numerical grade from 0 to 100: ");
+            long num = input.nextLong();
 
-        System.out.println("Number\tSquare\tCube");
-        for (int i=1; i <= num; i++) {
-            int square = i * i;
-            int cube = i * i * i;
-            System.out.println(i + "\t" + square + "\t" + cube);
-        }
-           System.out.println("Do you want to continue (Yes/No)");
-            choice = input.next();
-        } while (choice.equalsIgnoreCase("Yes"));
+                if (num >= 88 && num <= 100) {
+                    System.out.println("A");
+                } else if (num >= 80 && num <= 87) {
+                    System.out.println("B");
+                } else if (num >= 67 && num <= 79) {
+                    System.out.println("C");
+                } else if (num >= 60 && num <= 66) {
+                    System.out.println("D");
+                } else {
+                    System.out.println("F - You Failed");
+                }
+                System.out.println("Do you want to continue (Yes/No)");
+                choice = input.next();
+            }
+            while (choice.equalsIgnoreCase("Yes")) ;
         }
     }
 
 
-//        Display a table of powers.
+
 //
-//                Prompt the user to enter an integer.
-//        Display a table of squares and cubes from 1 to the value entered.
-//        Ask if the user wants to continue.
-//        Assume that the user will enter valid data.
-//        Only continue if the user agrees to.
+//Prompt the user for a numerical grade from 0 to 100.
+//Display the corresponding letter grade.
+//Prompt the user to continue.
+//Assume that the user will enter valid integers for the grades.
+//The application should only continue if the user agrees to.
+//Grade Ranges:
+
+//A : 100 - 88
+//B : 87 - 80
+//C : 79 - 67
+//D : 66 - 60
+//F : 59 - 0
