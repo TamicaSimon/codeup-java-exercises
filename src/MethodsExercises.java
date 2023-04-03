131277ecbf4167c3f4bca4bbec6ba4d21d9ca886
Tamica Simon <tamica.simon@gmail.com>
2023-04-03 16:14:39 +0000

    Dice Rolling

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -50,29 +50,29 @@ public class MethodsExercises {
 //    }
 //}
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-
-            do {
-                System.out.print("Enter an integer from 1 to 10 to calculate its factorial: ");
-                int n = scanner.nextInt();
-
-                if (n < 1 || n > 10) {
-                    System.out.println("Number should be between 1 and 10.");
-                } else {
-                    long factorial = 1;
-                    for (int i = 1; i <= n; i++) {
-                        factorial *= i;
-                    }
-                    System.out.printf("The factorial of %d is %d\n", n, factorial);
-                }
-
-                System.out.print("Do you want to continue? (y/n): ");
-            } while (scanner.next().equalsIgnoreCase("y"));
-
-            scanner.close();
-        }
-    }
+//        public static void main(String[] args) {
+//            Scanner scanner = new Scanner(System.in);
+//
+//            do {
+//                System.out.print("Enter an integer from 1 to 10 to calculate its factorial: ");
+//                int n = scanner.nextInt();
+//
+//                if (n < 1 || n > 10) {
+//                    System.out.println("Number should be between 1 and 10.");
+//                } else {
+//                    long factorial = 1;
+//                    for (int i = 1; i <= n; i++) {
+//                        factorial *= i;
+//                    }
+//                    System.out.printf("The factorial of %d is %d\n", n, factorial);
+//                }
+//
+//                System.out.print("Do you want to continue? (y/n): ");
+//            } while (scanner.next().equalsIgnoreCase("y"));
+//
+//            scanner.close();
+//        }
+//    }
 
 //    Calculate the factorial of a number.
 //
@@ -85,3 +85,36 @@ public class MethodsExercises {
 //        Continue only if the user agrees to.
 //        A factorial is a number multiplied by each of the numbers before it.
 //        Factorials are denoted by the exclamation point (n!). Ex:
+
+
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            int numSides;
+            do {
+                System.out.print("How many sides does a pair of dice have? ");
+                numSides = scanner.nextInt();
+            } while (numSides < 2);
+
+            String input;
+            do {
+                System.out.print("Press enter to roll the dice...");
+                scanner.nextLine();
+                int result1 = rollDice(numSides);
+                int result2 = rollDice(numSides);
+                System.out.println("You rolled a " + result1 + " and a " + result2);
+                System.out.print("Roll again? (y/n): ");
+                input = scanner.nextLine();
+            } while (input.equalsIgnoreCase("y"));
+        }
+
+        public static int rollDice(int numSides) {
+            return (int) (Math.random() * numSides) + 1;
+        }
+    }
+//    Create a java application that simulates dice rolling.
+//    Ask the user to enter the number of sides for a pair of dice.
+//    Prompt the user to roll the dice.
+//"Roll" two n-sided dice, display the results of each, and then ask the user if he/she wants to roll the dice again.
+//    Use static methods to implement the method(s) that generate the random numbers.
+//    Use the .random method of the java.lang.Math class to generate random numbers.
---
+++ src/Person.java
@@ -0,0 +1,23 @@
+public class Person {
+    private String name;
+    public String getName;
+
+}
+
+
+//Create Person class inside of src that has a private name field that is a string, and the following methods:
+//public String getName(){
+////TODO: return the person's name
+//}
+//
+//public void setName(String name){
+////TODO: change the name field to the passed value
+//}
+//public void sayHello(){
+////TODO: print a message to the console using the person's name
+//}
+//The class should have a constructor that accepts a `String` value and sets
+//the person's name to the passed string.
+//
+//Create a `main` method on the class that creates a new `Person` object and
+//tests the above methods.
